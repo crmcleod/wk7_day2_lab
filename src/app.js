4f872866951ef267755ff0012f01f6fbf2e46fc9
Craig McLeod <mcleod.craig@outlook.com>
2020-06-23 15:31:57 +0100

    Liveshare finished, lab over

---
+++ src/app.js
@@ -0,0 +1,34 @@
+import  Vue from 'vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Vue({
+      el: "#app",
+      data: {
+        countries: null
+      },
+      mounted(){
+        this.fetchCountries()
+      },
+
+      computed: {
+          totalPopulation: function(){
+              return this.countries.reduce((runningTotal, country) => {
+                  return runningTotal + country.population
+              }, 0);
+          }
+      },
+      
+      methods: {
+        fetchCountries: function(){
+          fetch("https://restcountries.eu/rest/v2/all")
+          .then(response => response.json())
+          .then(data => this.countries = data)
+        }
+        
+      }
+
+      
+  })
+  
+  
+})
